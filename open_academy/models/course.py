6064640e3fafbcaa46efc97aa222f37872b52c5c
RonyBarahona <barahonar50@gmail.com>
2022-02-23 12:46:17 -0800

    crear modelo sesion

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -11,4 +11,21 @@ class open_academy(models.Model):
     title = fields.Char(string='title')
     description = fields.Text(string='description')
 
-     
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+
+
+class session(models.Model):
+    _name = 'open_academy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="duracion en dias")
+    seats = fields.Integer(string="Cupos")     
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('open_academy.course',
+        ondelete='cascade', string="Course", required=True)
